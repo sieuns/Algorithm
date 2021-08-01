caa02c06f4ba0ce6f37cabf7117a98f00ae9ca36
양시은 <mn07020@gmail.com>
2021-08-01 19:46:17 +0900

    커밋 테스트

--- Data Structure Practice/chap02/Q2.java
+++ Data Structure Practice/chap02/Q2.java
@@ -21,7 +21,7 @@ public class Q2 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int[] x = {5,10,73,2,-5,42};
+		int[] x = {5,10,73,2,-5,44};
 		for(int i=0;i<x.length;i++) {
 		System.out.print(x[i]+ " ");
 		}System.out.println(" ");
